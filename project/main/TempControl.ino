2cf0345b977a4f37ba39a32dcae8b143f372bf8a
pekpuglia <pepuglia@gmail.com>
2023-08-17 15:37:16 +0200

    temp control functions

--- project/main/TempControl.ino
+++ project/main/TempControl.ino
@@ -1,3 +1,7 @@
+#include "OneWireSTM.h"
+#include "DallasTemperature.h"
+
+//dados p transmitir
 struct TempControlData
 {
     float internalTempC;
@@ -15,4 +19,52 @@ const long internalTemperatureMeasurementRateMillis = 5000;
 const float internalTemperatureGoal = 15.0;
 const float internalTemperatureTolerance = 2.0;
 
-bool onOff
+/*
+    Lógica de controle
+*/
+bool isOn = false;
+
+//calcula o comando necessário
+bool controlLogic(float measuredTemp) {
+    if (measuredTemp < internalTemperatureGoal - internalTemperatureTolerance)
+        return true;
+    if (measuredTemp > internalTemperatureGoal + internalTemperatureTolerance)
+        return false;
+
+    return isOn;
+}
+
+/*
+    Sensores de temperatura
+*/
+
+//colocar pinos certos!!!!!!
+OneWire payloadTempWire(PB8);
+OneWire externalTempWire(PB9);
+
+DallasTemperature payloadTempSensor(&payloadTempWire);
+DallasTemperature externalTempSensor(&externalTempWire);
+
+float internalTemperature() {
+    payloadTempSensor.requestTemperatures();
+    return payloadTempSensor.getTempCByIndex(0);
+}
+
+float externalTemperature() {
+    externalTempSensor.requestTemperatures();
+    return externalTempSensor.getTempCByIndex(0);
+}
+
+/*
+    Controle de potência
+*/
+
+//colocar pino certo!!!
+const uint8 transistorControlPin = 0;
+
+void controlOutput() {
+    if (isOn)
+        digitalWrite(transistorControlPin, HIGH);
+    else
+        digitalWrite(transistorControlPin, LOW);
+}
